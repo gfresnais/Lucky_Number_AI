7d1ef3858996869ca254485ee1a294600fe10428
Lilian.V <lilian.vallee1108@gmail.com>
2021-01-21 15:48:45 +0100

    Modification : Classe Joueur + Classe Pioche

--- App/src/Joueur.py
+++ App/src/Joueur.py
@@ -5,40 +5,85 @@ Created on Thu Jan  7 14:53:05 2021
 @author: vallee
 """
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from collections import deque
 
 
 class Joueur:
-    OLD_PLATEAU = np.eye(4, 4, int())
+    
+    OLD_PLATEAU = np.eye(4,4,int())
     JETON = 0
-
-    def __init__(self,
-                 firstNumber,
-                 secondNumber,
-                 thirdNumber,
+    
+    def __init__(self, 
+                 firstNumber, 
+                 secondNumber, 
+                 thirdNumber, 
                  fourthNumber):
-        setDeJeton = np.array([firstNumber, secondNumber, thirdNumber, fourthNumber])
+        setDeJeton = np.array([firstNumber,secondNumber,thirdNumber,fourthNumber])
         setDeJeton.sort()
-        self.OLD_PLATEAU[0, 0] = setDeJeton[0]
-        self.OLD_PLATEAU[1, 1] = setDeJeton[1]
-        self.OLD_PLATEAU[2, 2] = setDeJeton[2]
-        self.OLD_PLATEAU[3, 3] = setDeJeton[3]
-
+        self.OLD_PLATEAU[0,0] = setDeJeton[0]
+        self.OLD_PLATEAU[1,1] = setDeJeton[1]
+        self.OLD_PLATEAU[2,2] = setDeJeton[2]
+        self.OLD_PLATEAU[3,3] = setDeJeton[3]
+    
+    
     def verification(self, newPlateau):
         for colone in range(4):
             for ligne in range(4):
-                if (self.OLD_PLATEAU[colone, ligne] != newPlateau[colone, ligne]):
+                if(self.OLD_PLATEAU[colone,ligne] != newPlateau[colone, ligne]):
                     number = newPlateau[colone, ligne]
-                    if (colone != 0):
-                        if (number <= newPlateau[colone - 1, ligne]):
+                    if(colone != 0):
+                        if(number <= newPlateau[colone-1, ligne]):
                             return False
-                    if (ligne != 0):
-                        if (number <= newPlateau[colone, ligne - 1]):
+                    if(ligne != 0):
+                        if(number <= newPlateau[colone, ligne-1]):
                             return False
-                    if (colone != 3):
-                        if (number <= newPlateau[colone + 1, ligne]):
+                    if(colone != 3):
+                        if(number <= newPlateau[colone+1, ligne]):
                             return False
-                    if (ligne != 3):
-                        if (number <= newPlateau[colone, ligne + 1]):
+                    if(ligne != 3):
+                        if(number <= newPlateau[colone, ligne+1]):
                             return False
                     self.OLD_PLATEAU = newPlateau
                     return True
+
+    def getState(self):
+        state = []
+
+        for i in range(4):
+            state.append(self.OLD_PLATEAU[i])
+
+        state.append(self.JETON)
+
+        return state
+
+    def newJeton(self, jeton):
+        self.JETON = jeton
+
+
+
+class trainer:
+
+    def __init__(self, name=None, learning_rate=0.001, epsilon_decay=0.9999, batch_size=30, memory_size=3000):
+        self.state_size = 17
+        self.action_size = 16
+        self.gamma = 0.9
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.memory = deque(maxlen=memory_size)
+        self.batch_size = batch_size
+
+        #pas bon à refaire
+        model = Keras.Sequential()
+        model.add(Dense(50, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(30, activation='relu'))
+        model.add(Dense(30, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+
+        self.model = model
--- App/src/Pioche.py
+++ App/src/Pioche.py
@@ -8,28 +8,31 @@ Created on Mon Jan 11 14:00:44 2021
 import json
 import random
 
-
 class Pioche:
+    
     PIOCHE = []
     DEFAUSSE = []
-
+    
     def __init__(self):
+            
         self.PIOCHE = json.load('Assets/pioche.json')
         print(self.PIOCHE)
-
-    # fonction qui pioche un jeton dans la pioche et le retourne
-    def pioche(self):
+    
+    
+    #fonction qui pioche un jeton dans la pioche et le retourne
+    def piocheJeton(self):
         taille = len(self.PIOCHE)
-        index = random.Random() * taille
-
+        index = random.Random()*taille
+        
         return self.PIOCHE.pop(index)
-
-    # fonction qui pioche un jeton donné dans la défausse
-    def defausse(self, jeton):
+        
+    #fonction qui pioche un jeton donné dans la défausse
+    def piocheDefausse(self, jeton):
         index = self.PIOCHE.index(jeton)
-
+        
         return self.PIOCHE.pop(index)
-
-    # fonction qui ajoute un jeton à la défausse
-    def ajoutDefausse(self, jeton):
+    
+    #fonction qui ajoute un jeton à la défausse
+    def DefausseJeton(self, jeton):
         self.PIOCHE.append(jeton)
+    
