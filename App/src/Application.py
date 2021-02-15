f5e6b181d9e5bf83eaed88eea9f2d8293eb3cd62
Zineb <zineb.kabbab.03@gmail.com>
2021-02-15 12:29:16 +0100

    Index v2 ( plateau IA)

--- App/src/Application.py
+++ App/src/Application.py
@@ -14,9 +14,6 @@ def acceuil():
 
 @app.route('/index')
 def index():
-    game.joueur.add_jeton(game.pioche.piocheJeton())
-    game.joueur.add_jeton(game.pioche.piocheJeton())
-    game.joueur.add_jeton(game.pioche.piocheJeton())
     return render_template('index1.html', game=game)
 
 app.run()
